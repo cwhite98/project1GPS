72dc4c8db65345b30e6497b94dcc359312b2ee17
Camila White <camilawhite@MacBook-Pro-Camila.local>
2019-02-02 09:55:33 -0500

    Subiendo Ruta

--- app/public/js/route.js
+++ app/public/js/route.js
@@ -1,18 +1,38 @@
-var map;
 var points;
 
 function initMap() {
+    var map;
     points = JSON.parse(document.getElementById('points').value);
-    var lats = [];
-    var lons = [];
-    for(var i = 0; i < points.length; i++) {
-        lats[i] = points[i].lat;
-        lons[i] = points[i].lon;
-    }
-    console.log(lats);
-    console.log(lons);
+    console.log(points);
     map = new google.maps.Map(document.getElementById('map-canvas'), {
         center: new google.maps.LatLng(6.200367, -75.577609),
         zoom: 16
+    });
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    var waypts = [];
+    for (var i = 1; i < points.length-1; i++) {
+        waypts.push({
+            location: new google.maps.LatLng(points[i].lat, points[i].lon),
+            stopover: true
+        });
+    }
+    directionsService.route({
+        origin: new google.maps.LatLng(points[0].lat, points[0].lon),
+        destination: new google.maps.LatLng(points[points.length-1].lat, points[points.length-1].lon),
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: 'WALKING'
+    }, function (response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
     });
 }
